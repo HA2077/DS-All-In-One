91827d4d06b655313afb796ab6bbac03e452d600
Hassan Ahmed <hassanstar2007@gmail.com>
2026-02-24 03:05:56 +0200

    - Removed the executable file for Stack. - Made a minor change to Main.cpp. - Changed a bit on the pop() method to add a safe net.

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 /*
 MADE BY: HA
-This module implements the Stack DS using a vector the class got 3 methods:
+This module implements the Stack (INT NUMBERS ONLY) DS using a vector the class got 3 methods:
 1. push: to add an element to the top of the stack.
 2. pop: to remove the top element from the stack.
 3. printStack: to print the elements in the stack, the stack size and the top element.
@@ -16,26 +16,31 @@ class Stack{
     public:
         int top;
         int stacksize = 0;
+
         void push(int value){
             stack.push_back(value);
             top = value;
             stacksize++;
             cout << "Pushed: " << value << " to the stack." << endl;
         }
+
         void pop(){
+            if (stacksize == 0){
+            cout << "The stack is empty, Push an element first!" << endl;
+            return;
+            }
+
             stack.pop_back();
             stacksize--;
+
             if(stacksize > 0){
                 top = stack[stacksize - 1];
                 cout << "Popped The Top Element From the stack." << endl;
                 return;
             }
-            else{
-                top = -1;
-                cout << "The stack is empty, Push an element first!" << endl;
-                return;
-            }
+            else    top = -1;
         }
+
         void printStack(){
             cout << "Your Stack: " << endl;
             for (int i = 0;i < stacksize;++i)
